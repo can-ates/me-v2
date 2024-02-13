4edf2e485086163c274c99936939e9667b608d82
Mutlu Can ATES <canates.dev@gmail.com>
2024-02-13 14:43:12 +0300

    header padding

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import Socials from './Socials';
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full items-center px-16 xl:h-[90px] xl:px-0">
+    <header className="absolute z-30 w-full items-center md:px-16 xl:h-[90px] xl:px-0">
       <div className="container mx-auto">
         <div className="flex items-center justify-between gap-y-6 py-8 md:flex-row">
           {/* logo */}
