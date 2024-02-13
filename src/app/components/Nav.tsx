eb93f8e3748ce72be003e548ff0ccc753c8b667f
Mutlu Can ATES <canates.dev@gmail.com>
2024-02-13 23:06:20 +0300

    nav style update

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -17,7 +17,7 @@ const Nav = () => {
   return (
     <nav className="fixed bottom-0 top-0 z-50 mt-auto flex h-max w-full flex-col items-center gap-y-4 xl:right-[2%] xl:h-screen xl:w-16 xl:max-w-md xl:justify-center">
       {/* inner */}
-      <div className="flex h-[80px] w-full items-center justify-between gap-y-10 bg-white/10 px-4 py-8 text-3xl backdrop-blur-sm md:px-40 xl:h-max xl:flex-col xl:justify-center xl:rounded-full xl:px-0 xl:text-xl">
+      <div className="flex h-[60px] w-full items-center justify-between gap-y-10 bg-white/10 px-4 py-4 text-2xl backdrop-blur-sm md:h-[80px] md:px-40 md:py-8 md:text-3xl xl:h-max xl:flex-col xl:justify-center xl:rounded-full xl:px-0 xl:text-xl">
         {navData.map((link) => {
           return (
             <Link
